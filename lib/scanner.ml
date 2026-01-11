292ded82f36aaa58412de33ed576a6a253a2a635
Ruben Bakker <ruben@uncomplex.ch>
2026-01-11 14:46:26 +0100

    codecrafters submit [skip ci]

--- lib/scanner.ml
+++ lib/scanner.ml
@@ -1,22 +1,22 @@
 open Base
 
 type token_type = LEFT_PAREN | RIGHT_PAREN | LEFT_BRACE | RIGHT_BRACE | EOF
+type t = { type_ : token_type; lexeme : string }
 
 let char_list_of_string str = List.init (String.length str) ~f:(String.get str)
 
-let rec parse_rec (chars : char list) (acc : token_type list) : token_type list
-    =
+let rec parse_rec (chars : char list) (acc : t list) : t list =
   match chars with
-  | [] -> EOF :: acc
+  | [] -> { type_ = EOF; lexeme = "" } :: acc
   | char :: rest -> (
       match char with
-      | '(' -> parse_rec rest (LEFT_PAREN :: acc)
-      | ')' -> parse_rec rest (RIGHT_PAREN :: acc)
-      | '{' -> parse_rec rest (LEFT_BRACE :: acc)
-      | '}' -> parse_rec rest (RIGHT_BRACE :: acc)
+      | '(' -> parse_rec rest ({ type_ = LEFT_PAREN; lexeme = "(" } :: acc)
+      | ')' -> parse_rec rest ({ type_ = RIGHT_PAREN; lexeme = ")" } :: acc)
+      | '{' -> parse_rec rest ({ type_ = LEFT_BRACE; lexeme = "{" } :: acc)
+      | '}' -> parse_rec rest ({ type_ = RIGHT_BRACE; lexeme = "}" } :: acc)
       | _ -> parse_rec rest acc)
 
-let parse (chars : char list) : token_type list = parse_rec chars [] |> List.rev
+let parse (chars : char list) : t list = parse_rec chars [] |> List.rev
 
 let token_name (x : token_type) : string =
   match x with
@@ -26,18 +26,9 @@ let token_name (x : token_type) : string =
   | RIGHT_BRACE -> "RIGHT_BRACE"
   | EOF -> "EOF"
 
-let lexeme_for_token (x : token_type) : string =
-  match x with
-  | LEFT_PAREN -> "("
-  | RIGHT_PAREN -> ")"
-  | LEFT_BRACE -> "{"
-  | RIGHT_BRACE -> "}"
-  | EOF -> ""
+let t_to_string token =
+  Stdlib.Printf.sprintf "%s %s null\n" (token_name token.type_) token.lexeme
 
-let print_tokens (tokens : token_type list) =
-  List.map
-    ~f:(fun token ->
-      Stdlib.Printf.printf "%s %s null\n" (token_name token)
-        (lexeme_for_token token))
-    tokens
+let print_tokens (tokens : t list) =
+  List.map ~f:(fun token -> Stdlib.print_endline (t_to_string token)) tokens
   |> ignore
