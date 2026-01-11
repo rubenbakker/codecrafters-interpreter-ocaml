b72842893fd4a36fb52dd9d51e3f71990a67bce6
Ruben Bakker <ruben@uncomplex.ch>
2026-01-11 14:52:12 +0100

    codecrafters submit [skip ci]

--- lib/scanner.ml
+++ lib/scanner.ml
@@ -1,30 +1,35 @@
 open Base
 
 type token_type = LEFT_PAREN | RIGHT_PAREN | LEFT_BRACE | RIGHT_BRACE | EOF
-type t = { type_ : token_type; lexeme : string }
+type t = { token_type : token_type; lexeme : string }
 
 let char_list_of_string str = List.init (String.length str) ~f:(String.get str)
 
-let rec parse_rec (chars : char list) (acc : t list) : t list =
+let rec parse_rec (chars : char list) (acc : t list) (line : int) : t list =
   match chars with
-  | [] -> { type_ = EOF; lexeme = "" } :: acc
+  | [] -> { token_type = EOF; lexeme = "" } :: acc
   | char :: rest -> (
       match char with
       | '(' as v ->
           parse_rec rest
-            ({ type_ = LEFT_PAREN; lexeme = String.of_char v } :: acc)
+            ({ token_type = LEFT_PAREN; lexeme = String.of_char v } :: acc)
+            line
       | ')' as v ->
           parse_rec rest
-            ({ type_ = RIGHT_PAREN; lexeme = String.of_char v } :: acc)
+            ({ token_type = RIGHT_PAREN; lexeme = String.of_char v } :: acc)
+            line
       | '{' as v ->
           parse_rec rest
-            ({ type_ = LEFT_BRACE; lexeme = String.of_char v } :: acc)
+            ({ token_type = LEFT_BRACE; lexeme = String.of_char v } :: acc)
+            line
       | '}' as v ->
           parse_rec rest
-            ({ type_ = RIGHT_BRACE; lexeme = String.of_char v } :: acc)
-      | _ -> parse_rec rest acc)
+            ({ token_type = RIGHT_BRACE; lexeme = String.of_char v } :: acc)
+            line
+      | '\n' -> parse_rec rest acc (line + 1)
+      | _ -> parse_rec rest acc line)
 
-let parse (chars : char list) : t list = parse_rec chars [] |> List.rev
+let parse (chars : char list) : t list = parse_rec chars [] 1 |> List.rev
 
 let token_name (x : token_type) : string =
   match x with
@@ -35,7 +40,7 @@ let token_name (x : token_type) : string =
   | EOF -> "EOF"
 
 let t_to_string token =
-  Stdlib.Printf.sprintf "%s %s null" (token_name token.type_) token.lexeme
+  Stdlib.Printf.sprintf "%s %s null" (token_name token.token_type) token.lexeme
 
 let print_tokens (tokens : t list) =
   List.map ~f:(fun token -> Stdlib.print_endline (t_to_string token)) tokens
