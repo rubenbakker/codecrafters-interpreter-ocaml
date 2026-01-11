f43550f369e360c3753361a7e7ebe41717d02a1f
Ruben Bakker <ruben@uncomplex.ch>
2026-01-11 11:32:38 +0100

    codecrafters submit [skip ci]

--- .codecrafters/compile.sh
+++ .codecrafters/compile.sh
@@ -12,8 +12,10 @@ env
 echo ">>> before"
 opam list
 echo ">>> install"
-opam install ocaml_intrinsics_kernel sexplib0 base
+opam install --locked ocaml_intrinsics_kernel sexplib0 base
 echo ">>> after"
 opam list
+touch /app/testx
+ls -la /app/testx
 echo ">>> compile"
 dune build --build-dir /tmp/codecrafters-build-interpreter-ocaml
